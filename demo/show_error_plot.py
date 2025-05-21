badb051c2bd8f8b35b466f9e84435c08325acdc6
WolfgangKonen <WolfgangKonen@users.noreply.github.com>
2025-05-21 09:30:35 +0200

    Update 2025-05-21, error_plot.png

--- demo/show_error_plot.py
+++ demo/show_error_plot.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 from cobraInit import CobraInitializer
 from gCOP import GCOP
 
-def show_error_plot(cobra: CobraInitializer, gfunc: GCOP):
+def show_error_plot(cobra: CobraInitializer, gfunc: GCOP, file=None):
     err = cobra.sac_res['fbestArray'] - gfunc.fbest
     plt.plot(range(err.size), np.abs(err), 'r-', label='error')
     plt.title(gfunc.name, fontsize=20)
     plt.xlabel('func evals ', fontsize=16)
     plt.ylabel('error', fontsize=16)
     plt.subplot(111).set_yscale("log")
-    plt.savefig("error_plot.jpg")
+    if file is not None:
+        plt.savefig(file)
     plt.show()
 
